8f2a4911943024091f04d283b8efd0ee0a149c13
Jai Manjhi <jaimanjhi6@gmail.com>
2026-09-06 23:35:03 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (40.86%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        stack<int> st;
+
+        for(int asteroid : asteroids)
+        {
+            bool alive = true;
+
+            while(alive &&
+                  !st.empty() &&
+                  st.top() > 0 &&
+                  asteroid < 0)
+            {
+                if(abs(asteroid) > st.top())
+                {
+                    st.pop();
+                }
+                else if(abs(asteroid) == st.top())
+                {
+                    st.pop();
+                    alive = false;
+                }
+                else
+                {
+                    alive = false;
+                }
+            }
+
+            if(alive)
+                st.push(asteroid);
+        }
+
+        vector<int> ans(st.size());
+
+        for(int i = st.size()-1; i >= 0; i--)
+        {
+            ans[i] = st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
+
