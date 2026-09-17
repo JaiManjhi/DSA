b26591e6746ca02804c2b69b01c616c339f1c90e
Jai Manjhi <jaimanjhi6@gmail.com>
2026-09-17 23:12:35 +0530

    Time: 5 ms (69.50%) | Memory: 21.8 MB (20.05%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    unordered_map<long long,int> mp;
+    int ans = 0;
+
+    void dfs(TreeNode* root, long long currSum, int target) {
+        if(root == nullptr)
+            return;
+
+        currSum += root->val;
+
+        ans += mp[currSum - target];
+
+        mp[currSum]++;
+
+        dfs(root->left, currSum, target);
+        dfs(root->right, currSum, target);
+
+        mp[currSum]--;
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        mp[0] = 1;
+
+        dfs(root, 0, targetSum);
+
+        return ans;
+    }
+};
